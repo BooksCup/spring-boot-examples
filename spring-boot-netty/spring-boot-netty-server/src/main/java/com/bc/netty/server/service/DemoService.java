518d9d921c939692ae8973ce1999c2f7acfdfb49
BooksCup <812809680@qq.com>
2019-07-25 09:45:22 +0800

    delete spring-boot-netty

--- spring-boot-netty/spring-boot-netty-server/src/main/java/com/bc/netty/server/service/DemoService.java
+++
@@ -1,8 +0,0 @@
-package com.bc.netty.server.service;
-
-
-import com.bc.netty.common.exception.ErrorParamsException;
-
-public interface DemoService {
-    double division(int numberA, int numberB) throws ErrorParamsException;
-}
